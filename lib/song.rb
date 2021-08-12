6e2f2d7ef8c1a7717f3b2d457e3be5f7d265cd0a
Jelena <jelena.durovic@powerhrg.com>
2021-08-12 15:25:25 -0400

    commit tree f53ecaf79bbff3b34943bbb09d212ea75e0136e0

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,63 @@ class Song
   def save
     self.class.all << self
   end
+
+def self.create
+  song = Song.new
+  song.save
+  song
+  
+end 
+
+def self.new_by_name(song_name)
+  song = Song.new
+  song.name = song_name
+  song
+ 
+end
+
+def self.create_by_name(song_name)
+  song = Song.new
+  song.name = song_name
+  song.save
+  song 
+end 
+
+def self.find_by_name(song_name)
+  @@all.find {|song| song.name == song_name}
+end 
+
+def self.find_or_create_by_name(name)
+  find_by_name(name) || create_by_name(name)
+end 
+
+def self.alphabetical
+  @@all.sort_by {|song| song.name}
+end 
+
+def self.new_from_filename(file_name)
+  song = Song.new
+
+  data = file_name.split(/\ - |\./)
+
+  song.artist_name = data[0]
+  song.name = data[1]
+  
+  song
+end 
+
+def self.create_from_filename(file_name)
+ 
+  song = self.new_from_filename(file_name).save
+ song
+end 
+
+def self.destroy_all
+  self.all.clear
+end 
+
+
+
+
 
 end
